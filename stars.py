82d72f0d1b62216b704e6a0b6c8ee0081b7cb6e2
Katie Challoner <katie@challoner.com>
2018-07-22 17:12:06 +0100

    Remove initial accelerations

--- stars.py
+++ stars.py
@@ -60,22 +60,11 @@ cir2.setOutline(outline2)
 cir2.setFill(interior2)
 cir2.draw(win)
 
-# Initial body accelerations
-a1_mag = 0
-a1_dir = 0
-a2_mag = 0
-a2_dir = 0
-
 # Resolve vectors into x and y
 v1x = v1_mag * math.sin(v1_dir)
 v1y = v1_mag * math.cos(v1_dir)
 v2x = v2_mag * math.sin(v2_dir)
 v2y = v2_mag * math.cos(v2_dir)
-
-a1x = a1_mag * math.sin(a1_dir)
-a1y = a1_mag * math.cos(a1_dir)
-a2x = a2_mag * math.sin(a2_dir)
-a2y = a2_mag * math.cos(a2_dir)
 
 # Newton's Gravitational Constant
 G = 6.7 * 10 ** -11
